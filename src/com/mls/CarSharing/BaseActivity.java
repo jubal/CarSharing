c0603e7c61aedf414ef7ce5f9b3b046848874847
Jubal Hoo <jubal.hoo@gmail.com>
2012-03-21 07:26:53 +0800

    try to add dialog in BaseActivity.

--- src/com/mls/CarSharing/BaseActivity.java
+++ src/com/mls/CarSharing/BaseActivity.java
@@ -1,20 +1,31 @@
 package com.mls.CarSharing;
 
-import com.mobclick.android.MobclickAgent;
-
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Toast;
 
-public class BaseActivity extends Activity {
+import com.mobclick.android.MobclickAgent;
+
+public class BaseActivity extends Activity implements OnClickListener {
 	
 	private String mTagString;
+	private Context mApplicationContext;
+	
+	protected static final int DIALOG_MODE_SELECTION = 929;
+	protected static final int DIALOG_EXIT_CONFIRM = DIALOG_MODE_SELECTION + 1;
+	
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		mTagString=this.toString();
+		mApplicationContext=getApplicationContext();
 		MobclickAgent.onError(this);
 		log("onCreate");
 	}
@@ -49,6 +60,27 @@ public class BaseActivity extends Activity {
 	public void onDestroy(){
 		super.onDestroy();
 		log("onDestroy");
+	}
+	
+	@Override
+	protected Dialog onCreateDialog(int id){
+		switch (id) {
+		case DIALOG_MODE_SELECTION:
+			AlertDialog.Builder builder = new AlertDialog.Builder(this);
+			return builder.create();
+
+		default:
+			break;
+		}
+		return super.onCreateDialog(id);
+	}
+	
+	@Override
+	public void onClick(View view){
+		int id = view.getId();
+		if (id == 1) {
+			
+		}
 	}
 	
 	/*
