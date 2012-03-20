2672eac65bd66311bc4b7d83d3f8da6db8c66d63
Jubal Hoo <jubal.hoo@gmail.com>
2012-03-20 20:49:48 +0800

    add admob in activity.

--- src/com/mls/CarSharing/CarSharingActivity.java
+++ src/com/mls/CarSharing/CarSharingActivity.java
@@ -1,12 +1,36 @@
 package com.mls.CarSharing;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
-public class CarSharingActivity extends BaseActivity {
+import com.google.ads.AdRequest;
+import com.google.ads.AdSize;
+import com.google.ads.AdView;
+
+public class CarSharingActivity extends BaseActivity{
     /** Called when the activity is first created. */
+	private AdView adView;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        adView=new AdView(this, AdSize.BANNER, "a14f6871d20e495");
+        LinearLayout layout = (LinearLayout)findViewById(R.id.mainLayout);
+
+        // Add the adView to it
+        layout.addView(adView);
+
+        // Initiate a generic request to load it with an ad
+        adView.loadAd(new AdRequest());
     }
+    
+    @Override
+    public void onDestroy() {
+      if (adView != null) {
+        adView.destroy();
+      }
+      super.onDestroy();
+    }
+    
 }
